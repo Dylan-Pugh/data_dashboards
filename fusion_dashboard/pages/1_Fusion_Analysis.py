8f75a0e5c519cc3732f697d1aa8581234c5de03e
Dylan Pugh <dpugh@gmri.org>
2023-09-24 21:53:29 -0400

    Init fusion page

--- fusion_dashboard/pages/1_Fusion_Analysis.py
+++ fusion_dashboard/pages/1_Fusion_Analysis.py
@@ -5,6 +5,9 @@ from viz import display_functions
 from processing import fusion_functions
 
 
+st.set_page_config(layout='wide', page_icon=':dna:')
+
+
 def display_fusion_results(df):
     # Display table of fusions
     st.dataframe(df, use_container_width=True, hide_index=True)
@@ -20,22 +23,36 @@ def display_fusion_results(df):
 
 df = pd.read_csv('fusion_dashboard/data/current_dex.csv')
 
+load_fusions = False
+
+if os.path.exists('fusion_dashboard/data/possible_fusions.csv'):
+    # Initialize with current data, if it exists
+    current_selection = pd.read_csv('fusion_dashboard/data/possible_fusions.csv')
+    load_fusions = True
+
+    default_fusions = set(current_selection['Head'].tolist())
+    default_fusions = [mon.capitalize() for mon in default_fusions]
+
 options = df['NAME'].to_list()
 
-selection = st.multiselect(label='Select Input Pokémon', options=[opt.capitalize() for opt in options])
+selection = st.multiselect(
+    label='Select Input Pokémon',
+    options=[opt.capitalize() for opt in options],
+    default=default_fusions if default_fusions else None,
+)
 
 # Only include useful columns
 metric = st.selectbox(
     label='Metric to Prioritize (only for optimal fusions)',
     options=[
-            'Hp',
-            'Attack',
-            'Defense',
-            'Special Attack',
-            'Special Defense',
-            'Speed',
-            'Bst',
-            'Effective Delta',
+        'Hp',
+        'Attack',
+        'Defense',
+        'Special Attack',
+        'Special Defense',
+        'Speed',
+        'Bst',
+        'Effective Delta',
     ],
 )
 
@@ -56,14 +73,12 @@ if st.button('Find All Fusions'):
     df = pd.read_csv('fusion_dashboard/data/possible_fusions.csv')
     display_fusion_results(df)
 elif st.button('Find Optimal Fusions'):
-    fusion_functions.get_possible_fusions(user_selection)
+    fusion_functions.get_possible_fusions(user_selection, adjust_for_threat_score)
     all_fusions = pd.read_csv('fusion_dashboard/data/possible_fusions.csv')
 
     fusion_functions.get_optimal_fusions(all_fusions, prioritized_metric=metric)
 
     df = pd.read_csv('fusion_dashboard/data/optimal_fusions.csv')
     display_fusion_results(df)
-elif os.path.exists('fusion_dashboard/data/possible_fusions.csv'):
-    # Initialize with current data, if it exists
-    current_selection = pd.read_csv('fusion_dashboard/data/possible_fusions.csv')
+elif load_fusions:
     display_fusion_results(current_selection)
