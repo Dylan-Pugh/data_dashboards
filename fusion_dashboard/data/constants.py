04a02c5638865cee569ad8e64d3e795f4b88dad3
Dylan Pugh <dpugh@gmri.org>
2023-10-02 00:44:37 -0400

    Adding dangerous moves constants

--- fusion_dashboard/data/constants.py
+++ fusion_dashboard/data/constants.py
@@ -1,1 +1,2 @@
 TYPES = ['Electric', 'Psychic', 'Fire', 'Water', 'Ice', 'Fighting', 'Grass', 'Ground', 'Dark', 'Ghost', 'Bug', 'Poison', 'Steel', 'Flying', 'Normal', 'Fairy', 'Dragon']
+DANGEROUS_MOVES = ['sonic-boom', 'dragon-rage', 'seismic-toss']
