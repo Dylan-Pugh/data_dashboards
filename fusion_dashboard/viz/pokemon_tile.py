a9123f3fed3c157ed47ff9ca666125549ccefb62
Dylan Pugh <dpugh@gmri.org>
2023-10-27 21:47:14 -0400

    Add move selector to tile

--- fusion_dashboard/viz/pokemon_tile.py
+++ fusion_dashboard/viz/pokemon_tile.py
@@ -34,7 +34,7 @@ def st_pokemon_tile(pokemon_df):
         learnset = pokemon_df['Learnset']
 
         # Filter so we only see upcoming moves
-        filtered_dict = {key: value for key, value in learnset.items() if key > current_level}
+        filtered_dict = {key: value for key, value in learnset.items() if isinstance(key, int) and key > current_level}
 
         # Enumerate the dict
         enumerated_data = [(key, value) for key, value in filtered_dict.items()]
@@ -46,27 +46,25 @@ def st_pokemon_tile(pokemon_df):
         learnset_df = learnset_df.sort_values(by='Level', ascending=True)
         st.dataframe(data=learnset_df, use_container_width=True, hide_index=True)
 
-        # if pokemon_df['Evoline'].items()
         # Display Evoline
         st.markdown('**Upcoming Evolutions:**')
 
         evoline = pokemon_df['Evoline']
 
-        # Filter so we only see upcoming moves
+        # Filter so we only see upcoming evos
         filtered_evo_levels = {}
         other_evo_methods = {}
 
         for key, value in evoline.items():
-            if isinstance(key, int) and key > current_level and key < 90:
-                filtered_evo_levels[key] = value
-            elif isinstance(key, str):
+            if isinstance(key, str):
                 other_evo_methods[key] = value
             elif isinstance(key, int) and key > 90:
                 # This is to account for happiness scores
                 other_evo_methods[key] = value
+            else:
+                filtered_evo_levels[key] = value
 
         # Enumerate the dicts
-
         if filtered_evo_levels:
             enumerated_levels = [(key, value) for key, value in filtered_evo_levels.items()]
 
@@ -83,3 +81,12 @@ def st_pokemon_tile(pokemon_df):
             enumerated_methods = [(key, value) for key, value in other_evo_methods.items()]
             other_method_df = pd.DataFrame(enumerated_methods, columns=['Item/Happiness', 'Evolution'])
             st.dataframe(data=other_method_df, use_container_width=True, hide_index=True)
+
+        # Display move set selector
+        unpacked_moves = [move.capitalize() for sublist in learnset.values() if isinstance(sublist, list) for move in sublist]
+        st.multiselect(
+            label='Moveset',
+            options=unpacked_moves,
+            max_selections=4,
+            key=f'{pokemon_df["Head"].capitalize()}/{pokemon_df["Body"].capitalize()}_move_select',
+        )
