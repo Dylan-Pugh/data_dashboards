7c1425d8c8785fe64b559acf3aac0b6642907920
Dylan Pugh <dpugh@gmri.org>
2023-10-27 21:43:43 -0400

    Updates to static data

--- fusion_dashboard/data/constants.py
+++ fusion_dashboard/data/constants.py
@@ -1,2 +1,2 @@
-TYPES = ['Electric', 'Psychic', 'Fire', 'Water', 'Ice', 'Fighting', 'Grass', 'Ground', 'Dark', 'Ghost', 'Bug', 'Poison', 'Steel', 'Flying', 'Normal', 'Fairy', 'Dragon']
+TYPES = ['Electric', 'Psychic', 'Fire', 'Water', 'Ice', 'Fighting', 'Grass', 'Ground', 'Dark', 'Ghost', 'Bug', 'Poison', 'Steel', 'Flying', 'Normal', 'Fairy', 'Dragon', 'Rock']
 DANGEROUS_MOVES = ['sonic-boom', 'dragon-rage', 'seismic-toss']
