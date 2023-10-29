42cef1fc929a9704fa00b8802f59203639382228
Dylan Pugh <dpugh@gmri.org>
2023-10-29 14:42:19 -0400

    Adding level cap constants

--- fusion_dashboard/data/constants.py
+++ fusion_dashboard/data/constants.py
@@ -1,2 +1,21 @@
 TYPES = ['Electric', 'Psychic', 'Fire', 'Water', 'Ice', 'Fighting', 'Grass', 'Ground', 'Dark', 'Ghost', 'Bug', 'Poison', 'Steel', 'Flying', 'Normal', 'Fairy', 'Dragon', 'Rock']
 DANGEROUS_MOVES = ['sonic-boom', 'dragon-rage', 'seismic-toss']
+
+LEVEL_CAPS = {
+    'Pewter': 12,
+    'Cerulean': 22,
+    'Vermillion': 26,
+    'Celadon': 35,
+    'Fuschia': 38,
+    'Saffron': 45,
+    'Cinnabar': 51,
+    'Viridian': 54,
+    'Goldenrod': 58,
+    'Azalea': 62,
+    'Violet City': 63,
+    'Blackthorn': 66,
+    'Boon Island': 64,
+    'Ecruteak': 67,
+    'Mahogany': 65,
+    'Chrono Island': 68,
+}
