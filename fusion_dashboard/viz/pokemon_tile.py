b413e106e6500b5581e1dc59a74314964af28a5f
Dylan Pugh <dpugh@gmri.org>
2023-11-17 10:55:15 -0500

    Key to ID, filter evo levels

--- fusion_dashboard/viz/pokemon_tile.py
+++ fusion_dashboard/viz/pokemon_tile.py
@@ -3,22 +3,26 @@ import streamlit as st
 from viz.display_functions import display_sprite_with_fallback
 
 
-@st.cache_data
+# @st.cache_data
 def st_pokemon_tile(pokemon_df):
-    with st.expander(label=f'{pokemon_df["Head"].capitalize()}/{pokemon_df["Body"].capitalize()}'):
+    with st.expander(
+        label=f'{pokemon_df["Head"].capitalize()}/{pokemon_df["Body"].capitalize()}',
+    ):
         # Display the Pokemon image
         display_sprite_with_fallback(
-            head_pokedex_number=pokemon_df['Head Id'],
-            body_pokedex_number=pokemon_df['Body Id'],
+            head_pokedex_number=pokemon_df['Head ID'],
+            body_pokedex_number=pokemon_df['Body ID'],
         )
 
         # Display the Pokemon details
         if pokemon_df['Secondary Type']:
-            st.markdown(f'**Type:** {pokemon_df["Primary Type"].capitalize()}/{pokemon_df["Secondary Type"].capitalize()}')
+            st.markdown(
+                f'**Type:** {pokemon_df["Primary Type"].capitalize()}/{pokemon_df["Secondary Type"].capitalize()}',
+            )
         else:
             st.markdown(f'**Type:** {pokemon_df["Primary Type"].capitalize()}')
 
-        st.markdown(f'**BST:** {pokemon_df["Bst"]}')
+        st.markdown(f'**BST:** {pokemon_df["BST"]}')
 
         # Level selector
         current_level = st.number_input(
@@ -26,7 +30,7 @@ def st_pokemon_tile(pokemon_df):
             value=20,
             min_value=1,
             max_value=100,
-            key=f'{pokemon_df["Head"].capitalize()}/{pokemon_df["Body"].capitalize()}_level_select',
+            key=f'{pokemon_df["ID"]}_level_select',
         )
 
         st.markdown('**Upcoming Moves:**')
@@ -35,7 +39,11 @@ def st_pokemon_tile(pokemon_df):
         learnset = pokemon_df['Learnset']
 
         # Filter so we only see upcoming moves
-        filtered_dict = {key: value for key, value in learnset.items() if isinstance(key, int) and key > current_level}
+        filtered_dict = {
+            key: value
+            for key, value in learnset.items()
+            if isinstance(key, int) and key > current_level
+        }
 
         # Enumerate the dict
         enumerated_data = [(key, value) for key, value in filtered_dict.items()]
@@ -65,9 +73,18 @@ def st_pokemon_tile(pokemon_df):
             else:
                 filtered_evo_levels[key] = value
 
+        # Filter so we only see upcoming moves
+        filtered_evo_levels = {
+            key: value
+            for key, value in filtered_evo_levels.items()
+            if isinstance(key, int) and key > current_level
+        }
+
         # Enumerate the dicts
         if filtered_evo_levels:
-            enumerated_levels = [(key, value) for key, value in filtered_evo_levels.items()]
+            enumerated_levels = [
+                (key, value) for key, value in filtered_evo_levels.items()
+            ]
 
             # Create a DataFrame from the list
             evoline_df = pd.DataFrame(enumerated_levels, columns=['Level', 'Evolution'])
@@ -79,15 +96,29 @@ def st_pokemon_tile(pokemon_df):
             st.dataframe(data=evoline_df, use_container_width=True, hide_index=True)
 
         if other_evo_methods:
-            enumerated_methods = [(key, value) for key, value in other_evo_methods.items()]
-            other_method_df = pd.DataFrame(enumerated_methods, columns=['Item/Happiness', 'Evolution'])
-            st.dataframe(data=other_method_df, use_container_width=True, hide_index=True)
+            enumerated_methods = [
+                (key, value) for key, value in other_evo_methods.items()
+            ]
+            other_method_df = pd.DataFrame(
+                enumerated_methods,
+                columns=['Item/Happiness', 'Evolution'],
+            )
+            st.dataframe(
+                data=other_method_df,
+                use_container_width=True,
+                hide_index=True,
+            )
 
         # Display move set selector
-        unpacked_moves = [move.capitalize() for sublist in learnset.values() if isinstance(sublist, list) for move in sublist]
+        unpacked_moves = [
+            move.capitalize()
+            for sublist in learnset.values()
+            if isinstance(sublist, list)
+            for move in sublist
+        ]
         st.multiselect(
             label='Moveset',
             options=unpacked_moves,
             max_selections=4,
-            key=f'{pokemon_df["Head"].capitalize()}/{pokemon_df["Body"].capitalize()}_move_select',
+            key=f'{pokemon_df["ID"]}_move_select',
         )
